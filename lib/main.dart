26e924a460eaef12d8911f1c3986dc4f59832a3e
User PC <daniilbidenko5@gmail.com>
2025-01-18 23:49:56 +0600

    Сделал функцию которая складывает 2 числа

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
-final String name = 'Данил';
+import 'package:flutter/foundation.dart';
+void main () {
+    final String name = 'Данил';
 final int age = 16;
-final int number = 12;
-final double room = 13.1;
+final int a = 12;
+final double b = 13.1;
 final String book = 'АБВГдейка';
+
+
+summary () {
+    print(a + b);
+}
+ 
+summary();
+}
+
+
+
+
